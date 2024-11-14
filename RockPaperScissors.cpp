1d8fb2cac9f2ceddb0d99314bc4164ba7d4c3a01
Jan Ash <janash_37@hotmail.co.uk>
2024-11-14 16:11:27 +0000

    Add project files.

---
+++ RockPaperScissors.cpp
@@ -0,0 +1,22 @@
+// RockPaperScissors.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    string userInput = "";
+
+    cout << "Welcome, player, to this game of 'Rock, Paper, Scissors'!\n";
+    cout << "Which will you go for?\n";
+    cout << "Decide now!\n";
+
+    cin >> userInput;
+
+    cout << "You have chosen " << userInput << ". An excellent choice.\n";
+
+
+};

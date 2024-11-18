3880fb5a469fad8854321c86b0b355459c5f27d3
Jan Ash <janash_37@hotmail.co.uk>
2024-11-18 17:45:04 +0000

    increase complexity of the response to user input

--- RockPaperScissors.cpp
+++ RockPaperScissors.cpp
@@ -2,21 +2,36 @@
 //
 
 #include <iostream>
-
-using namespace std;
+#include <vector>
+#include <random>
 
 int main()
 {
 
-    string userInput = "";
+    std::string userInput = "";
+    std::vector<std::string> outputOptions = { "Rock","Paper", "Scissors" };
 
-    cout << "Welcome, player, to this game of 'Rock, Paper, Scissors'!\n";
-    cout << "Which will you go for?\n";
-    cout << "Decide now!\n";
+    std::cout << "Welcome, player, to this game of 'Rock, Paper, Scissors'!\n";
+    std::cout << "Which will you go for?\n";
+    std::cout << "Decide now!\n";
 
-    cin >> userInput;
+    std::cin >> userInput;
 
-    cout << "You have chosen " << userInput << ". An excellent choice.\n";
+    std::mt19937 rng(std::random_device{}());
+    std::uniform_int_distribution<size_t> distrib(0, outputOptions.size() - 1);
+
+    std::string randomOutput = outputOptions[distrib(rng)];
+
+    if (userInput != "Rock" && userInput != "Paper" && userInput != "Scissors") {
+        std::cout << "You chose " << userInput << ", which is NOT an expected input.\n";
+        std::cout << "The game explodes in a fiery mess.\n";
+        std::cout << "Next time, try choosing from either 'Rock', 'Paper', or 'Scissors'!\n";
+    }
+    else {
+        std::cout << "You chose " << userInput << ". An excellent choice.\n";
+        std::cout << "The computer counters with: " << randomOutput << ".\n";
+    }
+
 
 
 };
